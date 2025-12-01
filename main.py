6167fdfb6d6e3e54a420d3f79822dd1dd9dadf97
Bintang <167542355+Bintangavatar@users.noreply.github.com>
2025-12-01 10:24:02 +0700

    main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ st.markdown("""
 @st.cache_resource
 def load_model():
     try:
-        model = tf.keras.models.load_model("99%_traintestval_2kls.h5")
+        model = tf.keras.models.load_model("testing6040.h5")
         return model
     except Exception as e:
         st.error(f"❌ Gagal memuat model: {e}")
